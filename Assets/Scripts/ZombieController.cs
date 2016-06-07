65c7ea440cf2b40f67b440f812049cb77db6968d
tudortibu <tudortibu@gmail.com>
2016-06-07 14:48:15 -0400

    Van Can Now Move, Added Tags as well
    I was told not to use this

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -5,22 +5,22 @@ public class ZombieController : MonoBehaviour {
 
     Vector3 direction;
     public float speed;
-    public Van  van;
+    public GameObject  van;
     
 	// Use this for initialization
 	void Start () {
-	    
-
+        van = GameObject.FindGameObjectWithTag("Van");
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
         SpriteRenderer SR = van.GetComponent<SpriteRenderer>();
-        Vector3 destin = van.transform.position;
         Vector3 start = transform.position;
+        Vector3 destin = SR.bounds.ClosestPoint(start);
+
         if(!SR.bounds.Contains(start))
-        transform.position = Vector3.Slerp(start, destin, speed);
+        transform.position = Vector3.Lerp(start, destin, speed);
          
 	}
 }
