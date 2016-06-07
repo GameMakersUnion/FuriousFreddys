588b1a4ef340517771bcdccb8c2ba8f2cb078bcb
tudortibu <tudortibu@gmail.com>
2016-06-06 21:59:39 -0400

    Zoms now swarm car
    i was told not to use this part

---
+++ Assets/Scripts/ZombieController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieController : MonoBehaviour {
+
+    Vector3 direction;
+    public float speed;
+    public Van  van;
+
+	// Use this for initialization
+	void Start () {
+	    
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 destin = van.transform.position;
+        Vector3 start = transform.position;
+
+        transform.position = Vector3.Slerp(start, destin, speed);
+         
+	}
+}
