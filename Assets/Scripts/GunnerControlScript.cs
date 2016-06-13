78c62b385eed366bd03de235a9fe880d69841184
Tyler Zanon <tdzanon@gmail.com>
2016-06-13 19:24:03 -0400

    Player switching works! New Assets! trucks!

---
+++ Assets/Scripts/GunnerControlScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GunnerControlScript : PlayerControlScript {
+    
+	protected override void Start () {
+        base.Start();
+    }
+
+    public override void Move(int direction)
+    {
+        //rotate left or right
+        
+        tf.Rotate(0, 0, moveFactor * -direction);
+    }
+
+    public void Shoot()
+    {
+        //fire projectile
+    }
+
+}
---
+++ Assets/Scripts/PlayerControlScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class PlayerControlScript : MonoBehaviour {
+
+    public float moveFactor;
+
+    protected Transform tf;
+
+    protected virtual void Start()
+    {
+        tf = GetComponent<Transform>();
+    }
+
+    public abstract void Move(int direction);
+
+}
