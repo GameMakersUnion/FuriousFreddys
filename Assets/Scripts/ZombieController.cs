8d854f087b04efbae7af96faad0a2edd83a0d092
tudortibu <tudortibu@gmail.com>
2016-06-13 22:00:19 -0400

    Sprites now Change when touching the car
    Salt'n Peppa would approve

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -2,25 +2,36 @@
 using System.Collections;
 
 public class ZombieController : MonoBehaviour {
-
+   public Sprite Oface;
+    public Sprite Face;
     Vector3 direction;
     public float speed;
-   GameObject  van;
-    
-	// Use this for initialization
-	void Start () {
-        van = GameObject.FindGameObjectWithTag("Vehicle");
+   GameObject  vehicle;
+    SpriteRenderer ZSR;
+    SpriteRenderer SR;
+    // Use this for initialization
+    void Start () {
+        vehicle = GameObject.FindGameObjectWithTag("Vehicle");
+         ZSR = this.GetComponent<SpriteRenderer>();
+        SR = vehicle.GetComponent<SpriteRenderer>();
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        SpriteRenderer SR = van.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
         Vector3 start = transform.position;
+
+
         Vector3 destin = SR.bounds.ClosestPoint(start);
 
-        if(!SR.bounds.Contains(start))
-        transform.position = Vector3.Lerp(start, destin, speed);
-         
+        if (!SR.bounds.Contains(ZSR.bounds.ClosestPoint(destin)))
+        {
+            ZSR.sprite = Face;
+            transform.position = Vector3.Lerp(start, destin, speed);
+        }
+        else {
+            ZSR.sprite = Oface;
+        }
 	}
 }
