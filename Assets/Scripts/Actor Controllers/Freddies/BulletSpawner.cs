7330cab488f324a792cf471d09e0e0759cdedde4
Tyler Zanon <tdzanon@gmail.com>
2016-06-20 20:17:04 -0400

    something

---
+++ Assets/Scripts/Actor Controllers/Freddies/BulletSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+
+    protected Transform tf;
+
+	protected virtual void Start ()
+    {
+        tf = GetComponent<Transform>();
+	}
+
+    public virtual void Spawn(GameObject Projectile, GameObject MuzzleFlash)
+    {
+        Instantiate(MuzzleFlash, tf.position, tf.rotation);
+        Instantiate(Projectile, tf.position, tf.rotation);
+    }
+
+}
---
+++ Assets/Scripts/Actor Controllers/Freddies/PlayerWeaponScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerWeaponScript : MonoBehaviour
+{
+
+    public WeaponController[] WeaponScripts;
+
+    private GameObject CurrWeapon;
+    private WeaponController CurrWeaponScript;
+    private Transform tf;
+    private Dictionary<KeyCode, int> Keys = new Dictionary<KeyCode, int>();
+    
+	void Start ()
+    {
+        tf = GetComponent<Transform>();
+        Keys.Add(KeyCode.Alpha1, 0);
+        Keys.Add(KeyCode.Alpha2, 1);
+        Keys.Add(KeyCode.Alpha3, 2);
+
+        EquipWeapon(WeaponScripts[Keys[KeyCode.Alpha1]]);
+    }
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            CurrWeaponScript.Fire();
+	    if (Input.GetKeyDown(KeyCode.Alpha1))
+            EquipWeapon(WeaponScripts[Keys[KeyCode.Alpha1]]);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            EquipWeapon(WeaponScripts[Keys[KeyCode.Alpha2]]);
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            EquipWeapon(WeaponScripts[Keys[KeyCode.Alpha3]]);
+    }
+    
+    void EquipWeapon(WeaponController Weapon)
+    {
+        Destroy(CurrWeapon);
+        CurrWeapon = (GameObject)Instantiate(Weapon.gameObject, Vector3.zero, tf.rotation);
+        CurrWeapon.transform.parent = gameObject.transform;
+        CurrWeapon.transform.localPosition = Weapon.pos;
+        CurrWeaponScript = CurrWeapon.GetComponent<WeaponController>();
+    }
+
+}
