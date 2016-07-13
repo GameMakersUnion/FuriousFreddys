0d51026bba630e50763360aece32d04fac4ed05c
Victor <victor.nguyen@hotmail.com>
2016-07-13 11:50:26 -0400

    Working Gamepad Message to Player One, and other prototypes

---
+++ Assets/Scripts/AirConsole/GamepadController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+/*
+The component allows a gameobject to send messages to the gamepad
+-Victor
+*/
+public class GamepadController : MonoBehaviour {
+
+    public List<GameObject> gameObjects;
+
+	// Use this for initialization
+	void Awake () {
+    }
+
+
+    public void SendMessageToController(int player=0)
+    {
+        //Say Hi to the first controller in the GetControllerDeviceIds List.
+
+        //We cannot assume that the first controller's device ID is '1', because device 1 
+        //might have left and now the first controller in the list has a different ID.
+        //Never hardcode device IDs!
+        int idOfFirstController = AirConsole.instance.GetControllerDeviceIds()[0];
+
+        AirConsole.instance.Message(idOfFirstController, "Hey there, first controller!");
+
+        //Log to on-screen Console
+        //logWindow.text = logWindow.text.Insert(0, "Sent a message to first Controller \n \n");
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/AirConsole/GamepadReceiver.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+/*
+The component allows the current gameobject to receive messages from the gamepad
+-Victor
+*/
+
+public class GamepadReceiver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
