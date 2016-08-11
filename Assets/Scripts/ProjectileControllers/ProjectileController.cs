1097b9bc9400201f0d16a473ae5221318b63c4cb
Tyler Zanon <tdzanon@gmail.com>
2016-08-11 11:14:37 -0400

    Redid player aiming and changed yellow fred's temp sprite

--- Assets/Scripts/ProjectileControllers/ProjectileController.cs
+++ Assets/Scripts/ProjectileControllers/ProjectileController.cs
@@ -5,11 +5,26 @@ using System.Collections;
 public class ProjectileController : MonoBehaviour {
 
 	public int damage;
+    public float timeAlive;
 
-	void OnTriggerEnter2D(Collider2D coll) {
+    private float deathTime;
+
+    void Start()
+    {
+        deathTime = Time.time + timeAlive;
+    }
+
+    void Update()
+    {
+        if (Time.time > deathTime) Destroy(gameObject);
+    }
+
+	void OnTriggerEnter2D(Collider2D coll)
+    {
         if (coll.gameObject.tag == "Enemy")
         {
             //call take dmg method in zombie controller
+
             Destroy(gameObject);
         }
 
