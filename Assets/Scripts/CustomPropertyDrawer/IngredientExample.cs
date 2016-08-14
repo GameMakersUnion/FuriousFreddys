fe4253b2c66c605545df3e8580784f1abd6d5df7
Ian Nastajus <ian.nastajus@gmail.com>
2016-08-14 18:27:48 -0400

    sample Custom Property Drawer working

---
+++ Assets/Scripts/CustomPropertyDrawer/IngredientExample.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IngredientExample : MonoBehaviour {
+
+    public Ingredient potionResult;
+    public Ingredient[] potionIngredients;
+
+}
