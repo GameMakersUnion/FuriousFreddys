ead3f46c5c22e34ca62613a81210f1c382ca027d
Tyler Zanon <tdzanon@gmail.com>
2016-08-15 20:19:08 -0400

    Reloading and basic UI

--- Assets/Scripts/ActorControllers/Freddies/WeaponController.cs
+++ Assets/Scripts/ActorControllers/Freddies/WeaponController.cs
@@ -3,26 +3,15 @@
 public class WeaponController : MonoBehaviour
 {
 
-    private float nextFire;
-
     public int magSize;
     public float fireRate, reloadTime;
     public Vector3 pos; //the position of the weapon that looks the most 'proper'
     public GameObject Projectile, MuzzleFlash;
     public BulletSpawner Spawner;
-    
-	void Start ()
-    {
-        nextFire = 0;
-    }
 
     public void Fire()
     {
-        if (Time.time > nextFire)
-        {
-            Spawner.Spawn(Projectile, MuzzleFlash);
-            nextFire = Time.time + fireRate;
-        }
+        Spawner.Spawn(Projectile, MuzzleFlash);
     }
 
 }
