bcd9bf2520f566dac6eaf010a6800f991a327939
Victor <victor.nguyen@hotmail.com>
2016-08-24 20:22:56 -0400

    Working prototype player connect

---
+++ Assets/Scripts/AirConsole/PlayerManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class PlayerManager: MonoBehaviour
+{
+    //private List<int> badguys = new List<int>();
+
+    void Awake()
+    {
+        // register events
+        //AirConsole.instance.onReady += OnReady;
+        //AirConsole.instance.onMessage += OnMessage;
+        AirConsole.instance.onConnect += OnConnect;
+        //AirConsole.instance.onDisconnect += OnDisconnect;
+        //AirConsole.instance.onDeviceStateChange += OnDeviceStateChange;
+        //AirConsole.instance.onCustomDeviceStateChange += OnCustomDeviceStateChange;
+        //AirConsole.instance.onDeviceProfileChange += OnDeviceProfileChange;
+        //AirConsole.instance.onAdShow += OnAdShow;
+        //AirConsole.instance.onAdComplete += OnAdComplete;
+        //AirConsole.instance.onGameEnd += OnGameEnd;
+    }
+
+    void OnConnect(int device_id)
+    {
+        SetActivePlayers();//Reassign the player numbers
+        //Send player count to player spawner
+    }
+
+    void OnDisconnect(int device_id)
+    {
+        SetActivePlayers();//Reassign the player numbers
+        //Send player count to player spawner
+    }
+    /// <summary>
+    /// Set the players as active
+    /// </summary>
+    public void SetActivePlayers()
+    {
+        //Set the currently connected devices as the active players (assigning them a player number)
+        AirConsole.instance.SetActivePlayers();
+    }
+
+    /// <summary>
+    /// Gets all player device ids
+    /// </summary>
+    public List<int> GetPlayerDeviceIds()
+    {
+        return AirConsole.instance.GetControllerDeviceIds();
+    }
+
+    /// <summary>
+    /// Gets single player device id from a player number
+    /// </summary>
+    public int GetPlayerDeviceId(int player)
+    {
+        return AirConsole.instance.ConvertPlayerNumberToDeviceId(player);
+    }
+
+    void OnDestroy()
+    {
+
+        // unregister events
+        if (AirConsole.instance != null)
+        {
+            AirConsole.instance.onConnect -= OnConnect;
+            AirConsole.instance.onDisconnect -= OnDisconnect;
+         
+        }
+    }
+}
---
+++ Assets/Scripts/AirConsole/ReadyPanelManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+using UnityEngine.UI;
+
+public class ReadyPanelManager : MonoBehaviour {
+
+    public int playerNumber = 0; //Player Number
+    private int deviceId = 0;//0=Screen
+    private Image image;
+
+	// Use this for initialization
+	void Start () {
+        AirConsole.instance.onConnect += OnConnect;
+        AirConsole.instance.onDisconnect += OnDisconnect;
+        image = GetComponent<Image>();
+    }
+
+    void OnConnect(int device_id)
+    {
+        AirConsole.instance.SetActivePlayers();
+        int convertedPlayerNumber = AirConsole.instance.ConvertDeviceIdToPlayerNumber(device_id);
+        if (playerNumber == convertedPlayerNumber)
+        {
+            deviceId = convertedPlayerNumber;
+            if (image)
+                image.color = Color.red;
+        }
+        Debug.Log("Player Number:"+playerNumber);
+        Debug.Log("Coverted Player Number:"+convertedPlayerNumber);
+        Debug.Log("Device Number:" + device_id);
+
+    }
+
+    void OnDisconnect(int device_id)
+    {
+        int convertedPlayerNumber = AirConsole.instance.ConvertDeviceIdToPlayerNumber(device_id);
+        if (playerNumber == convertedPlayerNumber)
+        {
+            deviceId = convertedPlayerNumber;
+            if (image)
+                image.color = Color.white;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+  
+	}
+}
