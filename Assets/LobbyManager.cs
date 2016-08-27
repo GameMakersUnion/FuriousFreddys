027744157c380572108d96eb5322b5c894f8eafa
Victor <victor.nguyen@hotmail.com>
2016-08-27 15:53:41 -0400

    Added new truck and renamed things

---
+++ Assets/LobbyManager.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviour {
+
+    private List<LobbyPanel> panels;
+    private bool allReady = false;
+    private PlayerManager playerManager;
+    private int playerCount; //number of connected players
+    private float countDown;
+    private bool isCountDown;
+    public Text countDownText;
+
+	// Use this for initialization
+	void Start () {
+        panels = gameObject.GetComponentsInChildren<LobbyPanel>().ToList<LobbyPanel>();
+        playerManager = Utils.FindComponentOn<PlayerManager>("SingletonGodController");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        allReady = true;
+        //If all the panels are true
+        playerCount = 0;
+        foreach (LobbyPanel panel in panels)
+        {
+            //If player is connected
+            if (panel.playerConnected)
+            {
+                playerCount++;
+                if (!panel.playerReady)//If player is not ready
+                {
+                    allReady = false;
+                }
+            }
+        }
+        if (allReady && playerCount >= 2)
+        {
+            //Let player manager know players are ready
+            if (playerManager && playerManager.status != PlayerManager.playersStatus.READY)
+            {
+                playerManager.PlayersStatus(PlayerManager.playersStatus.READY);
+                //Start Countdown
+                isCountDown = true;
+                countDown = 3.0f;
+                countDownText.text = "Game begins in 3 seconds!";
+            }/*
+            else if(playerManager.status != PlayerManager.playersStatus.READY)
+            {
+                playerManager.PlayersStatus(PlayerManager.playersStatus.UNREADY);
+                //Stop Countdown
+                isCountDown = false;
+                countDownText.text = "Waiting for players to ready";
+            }*/
+
+
+        }else
+        {
+            
+            playerManager.PlayersStatus(PlayerManager.playersStatus.UNREADY);
+            isCountDown = false;
+            if (playerCount < 2)
+                countDownText.text = "You need at least 2 players";
+            else
+                countDownText.text = "Waiting for players to ready";
+        }
+
+        if (countDown >= 0 && isCountDown)
+        {
+            
+            countDown -= Time.deltaTime;
+            countDownText.text = "Game begins in " + countDown + " seconds!";
+            if (countDown == 0)
+            {
+                playerManager.PlayersStatus(PlayerManager.playersStatus.GO);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/AirConsole/LobbyPanel.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+using UnityEngine.UI;
+
+public class LobbyPanel : MonoBehaviour {
+
+    public int playerNumber = 0; //Player Number
+    private int deviceId = 0;//0=Screen
+    private Image image;
+    public bool playerReady;
+    public bool playerConnected;
+
+	// Use this for initialization
+	void Start () {
+        AirConsole.instance.onConnect += OnConnect;
+        AirConsole.instance.onDisconnect += OnDisconnect;
+        image = GetComponent<Image>();
+        playerReady = false;
+    }
+
+    void OnConnect(int device_id)
+    {
+        AirConsole.instance.SetActivePlayers();
+        int convertedPlayerNumber = AirConsole.instance.ConvertDeviceIdToPlayerNumber(device_id);
+        if (playerNumber == convertedPlayerNumber)
+        {
+            deviceId = convertedPlayerNumber;
+            if (image)
+                image.color = Color.red;
+            playerConnected = true;
+        }
+
+       
+    }
+
+    void OnDisconnect(int device_id)
+    {
+        int convertedPlayerNumber = AirConsole.instance.ConvertDeviceIdToPlayerNumber(device_id);
+        if (playerNumber == convertedPlayerNumber)
+        {
+            deviceId = convertedPlayerNumber;
+            if (image)
+                image.color = Color.white;
+            playerConnected = false;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+  
+	}
+
+    /// <summary>
+    /// Change Player Ready State
+    /// </summary>
+    public void ReadyPlayer()
+    {
+        playerReady = !playerReady;
+        if(playerReady)
+            image.color = Color.green;
+        else if (playerConnected)
+            image.color = Color.red;
+        else
+            image.color = Color.white;
+    }
+}
