99e3479dd32a14849db7b8b8e6ebac995955f2ab
Tyler Zanon <tdzanon@gmail.com>
2016-08-27 15:52:36 -0400

    minor updates

--- Assets/Scripts/ActorControllers/Freddies/PlayerControlScript.cs
+++ Assets/Scripts/ActorControllers/Freddies/PlayerControlScript.cs
@@ -2,12 +2,14 @@
 using UnityEngine;
 
 public abstract class PlayerControlScript : EntityControlScript {
+
     public int playerNumber = 0; //Their player number
 
 
 
     private bool keyPress;
     protected Transform tf;
+    private GamepadReceiver receiver;
 
     public override int Health
     {
@@ -21,6 +23,9 @@ public abstract class PlayerControlScript : EntityControlScript {
     {
         keyPress = false;
         tf = GetComponent<Transform>();
+        receiver = gameObject.AddComponent<GamepadReceiver>();
+        receiver.upButtonPressed.AddListener(UpButtonPressed);
+
     }
 
 	protected override void OnCollisionEnter2D(Collision2D col)
@@ -32,7 +37,12 @@ public abstract class PlayerControlScript : EntityControlScript {
 	{
 	
 	}
-    
+
+    protected void UpButtonPressed()
+    {
+        Debug.Log("up button pressed");
+    }
+
     public abstract void PerformAction();
 
 	//do nothing
