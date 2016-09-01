eaaa06fe89162d338a02005e4ee8815c72f735c7
Ian Nastajus <ian.nastajus@gmail.com>
2016-08-31 20:55:26 -0400

    damaging zombies fixed.
    trigger turned off bullet. Note it takes several hits to kill zombies, feel free to tweak hit points.

--- Assets/Scripts/ProjectileControllers/ProjectileController.cs
+++ Assets/Scripts/ProjectileControllers/ProjectileController.cs
@@ -18,44 +18,15 @@ public class ProjectileController : MonoBehaviour, DamageVisitor {
         if (Time.time > deathTime) Destroy(gameObject);
     }
 
-	//i moved this logic to be "receiving" instead of "generating"
-	//i changed the prefab from trigger to regular rigidbody
-	void OnTriggerEnter2D(Collider2D col)
-	{
-
-        if (!(col.gameObject.tag == "Vehicle" || col.gameObject.tag == "Cell" || col.gameObject.tag == "Bullet"))
-            Destroy(gameObject);
-
-        if (col.gameObject.tag == "Zombie")
-		{
-
-			//depreciated
-			//this manner of interaction has been depreciated by the visitor pattern
-			//also the zombie will be responsible for destroying itself when it's health hits zero, we shouldn't be invoking it's death from here.
-			//also the zombie will be responsible for updateHealth, this shouldn't be publically exposed.
-
-			//call take dmg method in zombie controller
-			//coll.GetComponent<MasterZombieScript>().updateHealth(-damage);
-			//Destroy(gameObject);
-		}
-	}
-    /*
 	void OnCollisionEnter2D(Collision2D col)
 	{
-        if (col.gameObject.tag == "Zombie")
+		//demorgan's law
+		if (!(col.gameObject.tag == "Vehicle" && col.gameObject.tag == "Cell" && col.gameObject.tag == "Bullet"))
 		{
-
-			//depreciated
-			//this manner of interaction has been depreciated by the visitor pattern
-			//also the zombie will be responsible for destroying itself when it's health hits zero, we shouldn't be invoking it's death from here.
-			//also the zombie will be responsible for updateHealth, this shouldn't be publically exposed.
-
-			//call take dmg method in zombie controller
-			//coll.GetComponent<MasterZombieScript>().updateHealth(-damage);
-			//Destroy(gameObject);
+			Destroy(gameObject);
 		}
+
 	}
-    */
     public virtual int CauseDamageTo(DamageVisitable visitable)
 	{
 		return damage;
