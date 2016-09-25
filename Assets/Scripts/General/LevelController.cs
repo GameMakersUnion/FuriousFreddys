352aabee5696ec5028ddb408ebfb745a0ba9ac76
Ian Nastajus <ian.nastajus@gmail.com>
2016-09-25 15:08:24 -0400

    Lukas' code for moving Dirt segments along Road segments
    I integrated succesfully except that it appears strangely centered instead around the road, so essentially bugged.

--- Assets/Scripts/General/LevelController.cs
+++ Assets/Scripts/General/LevelController.cs
@@ -9,10 +9,19 @@ public class LevelController : MonoBehaviour {
 
     public float ScrollSpeed { get { return scrollSpeed; } } //Limited accessibility deliberately
     private float scrollSpeed = 0.2f;  //The speed to scroll the roadSegments at
+	private bool isOnRoad = false;
+	private int levelLength = 5;    //Amount of roadSegments to pass until level ends
 
     private List<GameObject> roadSegments;  //The list of GameObjects to hold the roadSegmentPrefabs
     public GameObject roadSegmentPrefab;    //The roadSegmentPrefab which the each segment is made from
     private SpriteRenderer srRoadSegment;      //The SpriteRenderer of the roadSegmentPrefab, used to get bounds
+	private List<Collider2D> colRoadSegments;
+
+	private List<GameObject> dirtSegments;
+	public GameObject dirtSegmentPrefab;
+	private SpriteRenderer srDirtSegment;
+
+	private Collider2D colVehicle;
 
     private List<GameObject> rocks;  //The list of GameObjects to hold the rockPrefabs
     public GameObject rockPrefab;       //The rockPrefab
@@ -20,7 +29,11 @@ public class LevelController : MonoBehaviour {
     // Use this for initialization
     void Start () {
 		createGameBounds();
-        if (MoveRoad)
+		float cameraHeight = Camera.main.orthographicSize * 2.0f;
+		float cameraWidth = cameraHeight * Screen.width / Screen.height;
+
+		//Lists to hold GameObjects
+		if (MoveRoad)
         {
             createRoadSegments();
         }
@@ -35,8 +48,14 @@ public class LevelController : MonoBehaviour {
                 createRoadSegments();
             }
 
+			//Check what surface the vehicle is touching, adjust speed
+			checkGroundType();
+
             //Iterate through roadSegments and adjust the y position by newYPos
             moveRoadSegments();
+
+			//Iterate through dirtSegments and adjust the y position by newYPos
+			moveDirtSegments();
 
             //Generate obstacles randomly
             generateRock();
@@ -44,7 +63,9 @@ public class LevelController : MonoBehaviour {
             //Iterate through rocks and adjust the y position
             moveRocks();
 
-        }
+			//
+			checkIfLevelOver();
+		}
 
         //Hide or Show Road
         if (ShowRoad != roadSegments[0].GetComponent<SpriteRenderer>().enabled)
@@ -69,10 +90,16 @@ public class LevelController : MonoBehaviour {
     {
         //List to hold roadSegments
         roadSegments = new List<GameObject>();
-        rocks = new List<GameObject>();
+		dirtSegments = new List<GameObject>();
+		rocks = new List<GameObject>();
+		colRoadSegments = new List<Collider2D>();
 
-        //SpriteRenderer of the roadSegmentPreFab
-        srRoadSegment = roadSegmentPrefab.GetComponent<SpriteRenderer>();
+		//SpriteRenderer of the roadSegmentPrefab and dirtSegmentPrefab
+		srRoadSegment = roadSegmentPrefab.GetComponent<SpriteRenderer>();
+		srDirtSegment = dirtSegmentPrefab.GetComponent<SpriteRenderer>();
+
+		//Collider2D of the vehiclePrefab
+		colVehicle = GameObject.FindGameObjectWithTag("Vehicle").GetComponent<Collider2D>();
 
         //Calculate srRoadSegment's scale when fit to screen
         float cameraHeight = Camera.main.orthographicSize * 2.0f;
@@ -82,27 +109,18 @@ public class LevelController : MonoBehaviour {
         float unitHeight = sRoadSegment.textureRect.height / sRoadSegment.pixelsPerUnit;
         srRoadSegment.transform.localScale = new Vector3(cameraWidth / unitWidth, cameraHeight / unitHeight);
 
-        //Add first segment
-        GameObject roadSegment1 = Instantiate(roadSegmentPrefab);
-        roadSegment1.transform.position = new Vector3(roadSegment1.transform.position.x, roadSegment1.transform.position.y, roadSegment1.transform.position.z);
-        roadSegments.Add(roadSegment1);
+		float rSUnitWidth = sRoadSegment.textureRect.width / sRoadSegment.pixelsPerUnit;
+        float rSunitHeight = sRoadSegment.textureRect.height / sRoadSegment.pixelsPerUnit;
+        srRoadSegment.transform.localScale = new Vector3(cameraWidth / rSUnitWidth, cameraHeight / rSunitHeight);
 
-        //Add second segment and offset by size of previous segment
-        GameObject roadSegment2 = Instantiate(roadSegmentPrefab);
-        roadSegment2.transform.position = new Vector3(roadSegment2.transform.position.x, srRoadSegment.bounds.extents.y * 2.0f, roadSegment2.transform.position.z);
-        roadSegments.Add(roadSegment2);
+        //Calculate srDirtSegment's scale
+        Sprite sDirtSegment = srDirtSegment.sprite;
+        float dSUnitWidth = sDirtSegment.textureRect.width / sDirtSegment.pixelsPerUnit;
+        float dSUnitHeight = sDirtSegment.textureRect.height / sDirtSegment.pixelsPerUnit;
+        srDirtSegment.transform.localScale = new Vector3(cameraWidth / (dSUnitWidth*4), cameraHeight / (dSUnitHeight*4));
 
-        //Add third segment and offset by size of second segment
-        GameObject roadSegment3 = Instantiate(roadSegmentPrefab);
-        roadSegment3.transform.position = new Vector3(roadSegment3.transform.position.x, srRoadSegment.bounds.extents.y * 4.0f, roadSegment3.transform.position.z);
-        roadSegments.Add(roadSegment3);
-
-        //Add fourth segment and offset by size of second segment
-        GameObject roadSegment4 = Instantiate(roadSegmentPrefab);
-        roadSegment4.transform.position = new Vector3(roadSegment4.transform.position.x, srRoadSegment.bounds.extents.y * 6.0f, roadSegment4.transform.position.z);
-        roadSegments.Add(roadSegment4);
-
-        Debug.Log(roadSegments);
+        addRoadSegmentsToList();
+        addDirtSegmentsToList();
     }
 
     void generateRock()
@@ -145,7 +163,8 @@ public class LevelController : MonoBehaviour {
             if (road.transform.position.y <= srRoadSegment.bounds.extents.y * -4.0f)        //If road is below the origin by double it's length
             {
                 newYPos = road.transform.position.y + srRoadSegment.bounds.extents.y * 8.0f - scrollSpeed;     //newYPos is set to above the origin by double road's length 
-            }
+				levelLength--;
+			}
             else
             {
                 newYPos = road.transform.position.y - scrollSpeed;      //newYPos is set to scrollSpeed lower than road's current Y position
@@ -154,6 +173,56 @@ public class LevelController : MonoBehaviour {
         }
     }
 
+	void moveDirtSegments()
+	{
+		foreach (GameObject dirt in dirtSegments)
+		{
+			float newYPos;
+
+			if (dirt.transform.position.y <= srDirtSegment.bounds.extents.y * -4.0f)        //If rock is below the origin by double it's length
+			{
+				newYPos = dirt.transform.position.y + srDirtSegment.bounds.extents.y * 6.0f - scrollSpeed;     //newYPos is set to above the origin by double rock's length 
+			}
+			else
+			{
+				newYPos = dirt.transform.position.y - scrollSpeed;      //newYPos is set to scrollSpeed lower than rock's current Y position
+			}
+			dirt.transform.position = new Vector3(dirt.transform.position.x, newYPos, dirt.transform.position.z);   //Set rock's position to newYPos
+		}
+	}
+
+	void checkGroundType()
+	{
+		isOnRoad = false;
+
+		foreach (Collider2D colRoadSegment in colRoadSegments)
+		{
+			if (colVehicle.IsTouching(colRoadSegment))
+			{
+				isOnRoad = true;
+			}
+		}
+
+		if (isOnRoad)
+		{
+			scrollSpeed = 0.5f;
+		}
+		else
+		{
+			scrollSpeed = 0.2f;
+		}
+
+		Debug.Log(scrollSpeed);
+	}
+
+	void checkIfLevelOver()
+	{
+		if (levelLength <= 0)
+		{
+			Debug.Break();
+		}
+	}
+
     void setRoadSegementsVisibility(bool state)
     {
         foreach (GameObject road in roadSegments)
@@ -161,4 +230,53 @@ public class LevelController : MonoBehaviour {
             road.GetComponent<SpriteRenderer>().enabled = state;
         }
     }
+
+	//START METHODS
+	void addRoadSegmentsToList()
+	{
+		//Add first segment
+		GameObject roadSegment1 = Instantiate(roadSegmentPrefab);
+		roadSegment1.transform.position = new Vector3(roadSegment1.transform.position.x, roadSegment1.transform.position.y, roadSegment1.transform.position.z);
+		roadSegments.Add(roadSegment1);
+
+		//Add second segment and offset by size of previous segment
+		GameObject roadSegment2 = Instantiate(roadSegmentPrefab);
+		roadSegment2.transform.position = new Vector3(roadSegment2.transform.position.x, srRoadSegment.bounds.extents.y * 2.0f, roadSegment2.transform.position.z);
+		roadSegments.Add(roadSegment2);
+
+		//Add third segment and offset by size of second segment
+		GameObject roadSegment3 = Instantiate(roadSegmentPrefab);
+		roadSegment3.transform.position = new Vector3(roadSegment3.transform.position.x, srRoadSegment.bounds.extents.y * 4.0f, roadSegment3.transform.position.z);
+		roadSegments.Add(roadSegment3);
+
+		//Add fourth segment and offset by size of second segment
+		GameObject roadSegment4 = Instantiate(roadSegmentPrefab);
+		roadSegment4.transform.position = new Vector3(roadSegment4.transform.position.x, srRoadSegment.bounds.extents.y * 6.0f, roadSegment4.transform.position.z);
+		roadSegments.Add(roadSegment4);
+
+		//Add roadSegmentColliders to colRoadSegments
+		colRoadSegments.Add(roadSegment1.GetComponent<Collider2D>());
+		colRoadSegments.Add(roadSegment2.GetComponent<Collider2D>());
+		colRoadSegments.Add(roadSegment3.GetComponent<Collider2D>());
+		colRoadSegments.Add(roadSegment4.GetComponent<Collider2D>());
+	}
+
+	void addDirtSegmentsToList()
+	{
+		//Add first segment
+		GameObject dirtSegment1 = Instantiate(dirtSegmentPrefab);
+		dirtSegment1.transform.position = new Vector3(0.0f, 0.0f, dirtSegment1.transform.position.z);
+		dirtSegments.Add(dirtSegment1);
+
+		//Add second segment and offset by size of previous segment
+		GameObject dirtSegment2 = Instantiate(dirtSegmentPrefab);
+		dirtSegment2.transform.position = new Vector3(0.0f, srDirtSegment.bounds.extents.y * 2.0f, dirtSegment2.transform.position.z);
+		dirtSegments.Add(dirtSegment2);
+
+		//Add third segment and offset by size of previous segment
+		GameObject dirtSegment3 = Instantiate(dirtSegmentPrefab);
+		dirtSegment3.transform.position = new Vector3(0.0f, srDirtSegment.bounds.extents.y * 4.0f, dirtSegment3.transform.position.z);
+		dirtSegments.Add(dirtSegment3);
+	}
+
 }
